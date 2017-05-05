d701a9baa676e2c66319abbb94b462d4832fe65f
sgljon10 <lukedanualjones@hotmail.co.uk>
2017-05-05 11:09:12 +0100

    Final

---
+++ MAIN/Accelerator.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Arrays; 
+
+class Accelerator
+{
+    static BufferedReader keyboard = new BufferedReader (new InputStreamReader(System.in)) ;
+    static PrintWriter screen = new PrintWriter( System.out, true);
+    static Random randGen = new Random(); 
+    
+    static final int number_of_muons = 2; 
+    static final int nmax = 200; //Used to determine the amount of points for the electrons path
+    
+    static final double muonmass = 105.6583745; //MeV
+    static final double c = 299792458;    
+    static final double magnetic_field = 1.45;
+    static final double radius = 7.112; 
+    static final double magic_momentum = 29.3; // lorenz factor for things to cancel out
+    static final double muon_momentum = Math.sqrt(Math.pow(muonmass,2)*(Math.pow(magic_momentum,2)-1)); //muon momnetum that will be measured, all others will be noise    
+    static final double muon_energy = magic_momentum*muonmass;
+    static final double mean_lifetime = 2.1969811E-6;
+    static final double circumference = 2*Math.PI*radius;
+    static final double speed = c*Math.sqrt(1-(1/Math.pow(magic_momentum,2)));
+    static final double positron_mass = 0.5109989461; //MeV   
+    static final double positron_charge = 1.6021766208E-19; 
+    static double [] finalX = new double [number_of_muons];
+    static double [] finalY = new double [number_of_muons];
+    static double [] finaltime = new double [number_of_muons];
+    static double [][] positron_X = new double [number_of_muons] [nmax];
+    static double [][] positron_Y = new double [number_of_muons] [nmax]; 
+    
+    private static void WriteToCircle() throws IOException {
+        FileWriter file = new FileWriter("circle.csv");  
+        PrintWriter outputFile = new PrintWriter("circle.csv");         
+        for (int n = 0; n < 1000; n++) {             
+        outputFile.println((n+1) + "," + radius*Math.cos(2*n*Math.PI/999) + "," + radius*Math.sin(2*n*Math.PI/999));
+        }    
+       outputFile.close();
+       screen.println("Data written to disk in file " + "circle.csv");
+       return;
+    }
+    
+    private static void WriteToDecay() throws IOException {
+        FileWriter file = new FileWriter("decay.csv");  
+        PrintWriter outputFile = new PrintWriter("decay.csv");         
+        for (int n = 0; n < number_of_muons; n++) {             
+        outputFile.println((n+1) + "," + finalX [n] + "," + finalY [n] + "," + finaltime [n]);
+        }    
+       outputFile.close();
+       screen.println("Data written to disk in file " + "decay.csv");
+       return;
+    }
+        
+    private static void WriteToPositron() throws IOException {
+        FileWriter file = new FileWriter("positron.csv");  
+        PrintWriter outputFile = new PrintWriter("positron.csv");
+       for (int i =0; i < number_of_muons; i++) { 
+        for (int n = 0; n < nmax; n++) {             
+        outputFile.println((n+1) + "," + positron_X [i][n] + "," + positron_Y [i][n]);
+        }
+       }
+       outputFile.close();
+       screen.println("Data written to disk in file " + "positron.csv");
+       return;
+    }
+
+      public static void main (String [] args) throws IOException
+    {
+    for (int i =0; i < number_of_muons; i++) {         
+        double lifetime = -mean_lifetime*Math.log(randGen.nextDouble());  
+        double distance_travelled = speed*lifetime;    
+        double loops = distance_travelled/circumference;
+        screen.println("The amounts of loops muon " + (i+1) + " did is " + loops);
+        finalX [i] = radius*Math.cos(distance_travelled/radius);
+        finalY [i] = radius*Math.sin(distance_travelled/radius);
+        finaltime [i] = lifetime;
+        double positron_energy = muon_energy *0.6; //for now, this will be chnaged
+        double positron_momentum = Math.sqrt(Math.pow(positron_energy,2) - Math.pow(positron_mass,2));        
+        double positron_path_radius = ((positron_momentum*positron_charge*1E6)/c)/(magnetic_field*positron_charge); //without use of lorentz as lorentz is very high
+        double positron_radiusdifference = radius - positron_path_radius;
+        double positron_pathcentre_x = positron_radiusdifference*Math.cos(distance_travelled/radius);
+        double positron_pathcentre_y = positron_radiusdifference*Math.sin(distance_travelled/radius);       
+        for (int n=0; n < nmax; n++) {
+            positron_X [i][n] = positron_path_radius*Math.cos((((double)n*360/(double)nmax)*Math.PI)/180) + positron_pathcentre_x;
+            positron_Y [i][n] = positron_path_radius*Math.sin((((double)n*360/(double)nmax)*Math.PI)/180) + positron_pathcentre_y;
+        }
+    }    
+    WriteToDecay(); 
+    WriteToCircle();
+    WriteToPositron();  
+    }
+    
+}
